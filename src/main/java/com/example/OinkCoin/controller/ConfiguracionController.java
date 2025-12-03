399ce5476737641302f8f0389c9d071e6d51017e
SAMUEL CASTILLO <155770020+cxstiboi@users.noreply.github.com>
2025-12-03 06:42:40 -0500

    AAAAAAAAA

--- src/main/java/com/example/OinkCoin/controller/ConfiguracionController.java
+++ src/main/java/com/example/OinkCoin/controller/ConfiguracionController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,13 +14,13 @@ import com.example.OinkCoin.service.UsuarioService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/configuracion") 
+
 public class ConfiguracionController {
 
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping
+    @GetMapping("/configuracion")
     public String mostrarConfiguracion(HttpSession session, Model model) {
         Usuario usuario = (Usuario) session.getAttribute("usuarioLogueado");
         
